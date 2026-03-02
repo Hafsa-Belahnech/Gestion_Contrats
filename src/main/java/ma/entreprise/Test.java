b2ed0c39c6b65c50f55a203a584db66eb0586dca
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-02 12:11:55 +0000

    Update Test.java

--- src/main/java/ma/entreprise/Test.java
+++ src/main/java/ma/entreprise/Test.java
@@ -6,14 +6,12 @@ public class Test {
     public static void main(String[] args) {
 
         try {
-            // L'appel au Singleton suffit à déclencher la génération des tables
             HibernateUtil.getSessionFactory();
             System.out.println("Succès : La base de données et les tables (Assurance, Client, Contrat) ont été générées.");
         } catch (Exception e) {
             System.err.println("Erreur lors de la génération : " + e.getMessage());
             e.printStackTrace();
         } finally {
-            // Toujours fermer la session factory à la fin du test
             HibernateUtil.shutdown();
         }
     }
